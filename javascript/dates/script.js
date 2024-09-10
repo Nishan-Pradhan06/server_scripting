ebb243461ecdc4493fafbe9785cce6ec76e6aa27
nishanpradhan <nishanpradhan500@gmail.com>
2024-09-10 10:04:33 +0545

    presentation-dates

--- javascript/dates/script.js
+++ javascript/dates/script.js
@@ -1,45 +1,144 @@
-//creating date objects
+// //creating date objects
 
-let currentDate = new Date();
-console.log(currentDate);  // Returns the current date and time
+// let currentDate = new Date();
+// console.log(currentDate);  // Returns the current date and time
 
-let specificDate = new Date(2024, 8, 9);  // September 9, 2024
-console.log(specificDate);
+// let specificDate = new Date(2024, 8, 9);  // September 9, 2024
+// console.log(specificDate);
 
-let dateFromString = new Date("September 9, 2024");
-console.log(dateFromString);
-
-
-let epochDate = new Date(0);  // Returns Jan 1, 1970
-console.log(epochDate)
-
-//dates method in js
-//getDate
-let date = new Date();
-console.log(date.getDate());  // Output: Day of the month (e.g., 9)
-
-//get month
-let getMonthDate = new Date();
-console.log(getMonthDate.getMonth());  // Output: Month (0 = January, 1 = February, etc.)
-
-//get year
-let dates = new Date();
-console.log(dates.getFullYear());  // Output: Full year (e.g., 2024)
-
-//hours
-let hrsDate = new Date();
-console.log(hrsDate.getHours());  // Output: Hour (0 to 23)
-
-///minutes
-let minDate = new Date();
-console.log(minDate.getMinutes());  // Output: Minutes (0 to 59)
-
-//seconds
-let secDate = new Date();
-console.log(secDate.getSeconds());  // Output: Seconds (0 to 59)
+// let dateFromString = new Date("September 9, 2024");
+// console.log(dateFromString);
 
 
-//getday
+// let epochDate = new Date(0);  // Returns Jan 1, 1970
+// console.log(epochDate)
 
-let dayDate = new Date();
-console.log(dayDate.getDay());  // Output: Day of the week (0 = Sunday, 1 = Monday, etc.)
+// //dates method in js
+// //getDate
+// let date = new Date();
+// console.log(date.getDate());  // Output: Day of the month (e.g., 9)
+
+// //get month
+// let getMonthDate = new Date();
+// console.log(getMonthDate.getMonth());  // Output: Month (0 = January, 1 = February, etc.)
+
+// //get year
+// let dates = new Date();
+// console.log(dates.getFullYear());  // Output: Full year (e.g., 2024)
+
+// //hours
+// let hrsDate = new Date();
+// console.log(hrsDate.getHours());  // Output: Hour (0 to 23)
+
+// ///minutes
+// let minDate = new Date();
+// console.log(minDate.getMinutes());  // Output: Minutes (0 to 59)
+
+// //seconds
+// let secDate = new Date();
+// console.log(secDate.getSeconds());  // Output: Seconds (0 to 59)
+
+
+// //getday
+
+// let dayDate = new Date();
+// console.log(dayDate.getDay());  // Output: Day of the week (0 = Sunday, 1 = Monday, etc.)
+
+
+// //setdate
+// let setdate =  new Date();
+// date.setDate(15);  // Sets the day of the month to 15
+// console.log(setdate);  // Example: Wed Sep 15 2024
+
+// //month
+// let mondate = new Date();
+// date.setMonth(5);  // Sets the month to June (since 0 = January)
+// console.log(mondate);  // Example: Mon Jun 09 2024
+
+
+// //fullyear
+// let fullYdate = new Date();
+// date.setFullYear(2025);  // Sets the year to 2025
+// console.log(fullYdate);  // Example: Tue Sep 09 2025
+
+// //min
+// let Hdate = new Date();
+// date.setHours(18);  // Sets the time to 6 PM
+// console.log(Hdate);  // Example: Tue Sep 09 2024 18:00:00
+
+// //setmin
+// let setmin = new Date();
+// date.setMinutes(30);  // Sets the minutes to 30
+// console.log(setmin);  // Example: Tue Sep 09 2024 18:30:00
+
+// //setsec
+// let seconddate = new Date();
+// date.setSeconds(45);  // Sets the seconds to 45
+// console.log(seconddate);  // Example: Tue Sep 09 2024 18:30:45
+
+// //set milisec
+// let milisec = new Date();
+// date.setMilliseconds(500);  // Sets the milliseconds to 500
+// console.log(milisec);  // Example: Tue Sep 09 2024 18:30:45.500
+
+// //setstimw
+// let setTimedate = new Date();
+// date.setTime(1609459200000);  // Sets the time to a specific timestamp (Jan 1, 2021)
+// console.log(setTimedate);  // Example: Fri Jan 01 2021 00:00:00
+
+
+// let date = new Date();
+// console.log(date)
+
+// let specificdate = new Date(2023, 9, 16);
+// console.log(specificdate)
+
+// let stringDate = new Date("Sept 9, 2024");
+// console.log(stringDate)
+
+// let epoch = new Date(0);
+// console.log(epoch) // Output: Thu Jan 01 1970 00:00:00
+
+
+// let todaydate = new Date();
+// console.log(todaydate.getDate());
+
+
+// let month = new Date();
+// console.log(month.getMonth());
+
+// let fulYear = new Date();
+// console.log(fulYear.getFullYear());
+
+// let hours = new Date();
+// console.log(hours.getHours());
+// let min = new Date();
+// console.log(min.getMinutes());
+// let sec = new Date();
+// console.log(sec.getSeconds());
+// let week = new Date();
+// console.log(week.getDay());
+// let set = new Date()
+// set.setDate();
+// console.log(set);
+
+// let setMonth = new Date()
+// setMonth.setMonth(1);
+// console.log(setMonth);
+// let setMonth = new Date()
+// setMonth.setMonth(1);
+// console.log(setMonth);
+
+// const month = ["january", "feb"];
+
+// let FUllyear = new Date();
+// FUllyear.setFullYear(2025);
+// // console.log(FUllyear);
+// let setHrs = new Date();
+// setHrs.setHours(12);
+// console.log(setHrs);
+
+let setMIn = new Date();
+console.log(setMIn);
+setMIn.setMinutes(30);
+console.log(setMIn);
