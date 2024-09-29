5f4348a4a963d756e2bdd3cd53e64c1e15ae89cf
nishanpradhan <nishanpradhan500@gmail.com>
2024-09-29 08:01:55 +0545

    todo-dom

--- javascript/DOM/todo/todo.js
+++ javascript/DOM/todo/todo.js
@@ -1,10 +1,47 @@
-const addbtn = document.querySelector("#addbtn")
-const del = document.querySelector("#task")
-const edit = document.querySelector("#task")
-const mac = document.querySelector("#task")
+const taskForm = document.querySelector("#taskForm");
+const taskInput = document.querySelector("#taskInput");
+const taskList = document.querySelector("#taskList");
 
-addbtn.addEventListener('click', function ()
-{
-    const list = task.value.trim();
-    console.log(list);
+taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const taskValue = taskInput.value.trim();
+    if (taskValue) {
+        addTask(taskValue);
+        taskInput.value = " ";
+    }
 })
+
+function addTask(task) {
+    const row = document.createElement("tr");
+    row.innerHTML =
+        `<th scope="row" class="task-item">${task}</th>
+                <td>
+                    <button class="btn btn-success complete-btn">Complete</button>
+                    <button class="btn btn-danger delete-btn">Delete</button>
+                </td>
+                `;
+    //append task
+    taskList.appendChild(row);
+    // add event for compelte btn
+    row.querySelector(".complete-btn").addEventListener("click", function () {
+        markComplete(row);
+    })
+    //add event for delete
+    row.querySelector(".delete-btn").addEventListener("click", function () {
+        deleteTask(row);
+    })
+
+}
+
+
+function markComplete(row) {
+    const taskItem = row.querySelector(".task-item");
+    taskItem.style.textDecoration = "line-through";
+    taskItem.style.color = "grey";
+    row.querySelector(".complete-btn").disabled = true;
+    window.alert("Mark as Completed.");
+}
+
+function deleteTask(row) {
+    row.remove();
+}
