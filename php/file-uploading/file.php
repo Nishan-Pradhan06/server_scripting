5df730528a74a6fbe355297eba95c2cc9915765a
nishanpradhan <nishanpradhan500@gmail.com>
2024-12-30 09:34:47 +0545

    Enhance file upload validation by checking file type and size limits

--- php/file-uploading/file.php
+++ php/file-uploading/file.php
@@ -17,18 +17,40 @@
 </html>
 <?php
 if (isset($_FILES['image'])) {
-    echo "<pre>";
-    print_r($_FILES['image']);
-    echo "</pre>";
+    $allowed_types = ['image/png', 'image/jpeg', 'image/gif'];
+    $max_size = 2 * 1024 * 1024; // 2 MB
 
     $file_name = $_FILES['image']['name'];
     $file_temp = $_FILES['image']['tmp_name'];
+    $file_size = $_FILES['image']['size'];
+    $file_type = $_FILES['image']['type'];
 
-   if( move_uploaded_file($file_temp, "upload-images/" . $file_name))
-   {
-    echo "File upload sucessfully....";
-    echo "<img src='upload-images/$file_name'/>";
-   }
-    
+    if (!in_array($file_type, $allowed_types)) {
+        echo "Error: Only PNG, JPG, and GIF files are allowed.";
+    } elseif ($file_size > $max_size) {
+        echo "Error: File size must be less than 2 MB.";
+    } else {
+        if (move_uploaded_file($file_temp, "upload-images/" . $file_name)) {
+            echo "File uploaded successfully.";
+            echo "<img src='upload-images/$file_name' alt='Uploaded Image'/>";
+        } else {
+            echo "Error: There was a problem uploading your file.";
+        }
+    }
 }
+
+
+// if (isset($_FILES['image'])) {
+//     echo "<pre>";
+//     print_r($_FILES['image']);
+//     echo "</pre>";
+
+//     $file_name = $_FILES['image']['name'];
+//     $file_temp = $_FILES['image']['tmp_name'];
+
+//     if (move_uploaded_file($file_temp, "upload-images/" . $file_name)) {
+//         echo "File upload sucessfully....";
+//         echo "<img src='upload-images/$file_name'/>";
+//     }
+// }
 ?>
